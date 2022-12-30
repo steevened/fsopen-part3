818d177f1e2ca32870745c488a1b6ff7d1af95f3
steven <alvaradosteven315@gmail.com>
2022-12-30 12:31:16 -0500

    installing cors dependencie

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+app.use(cors())
 
 let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
   },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
   },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
   },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
 ]
 
-
 app.use(express.json())
-
-
 
 const date = new Date()
 
 const newId = () => {
-  const ids = persons.map(person => person.id)
-  const maxId = persons.length > 0 
-    ? Math.max(...ids)
-    : 0
+  const ids = persons.map((person) => person.id)
+  const maxId = persons.length > 0 ? Math.max(...ids) : 0
   return maxId + 1
 }
 
@@ -49,60 +46,59 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/api/info', (req, res) => {
-  res.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${date}</p> `)
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p> <p>${date}</p> `
+  )
 })
-
 
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
-  const person = persons.find(p => p.id === id)
+  const person = persons.find((p) => p.id === id)
 
-  if(person) {
-    res.json(person)    
-  }else {
+  if (person) {
+    res.json(person)
+  } else {
     res.status(404).end()
   }
 })
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
-  const isNumberRepeated = persons.find(person => person.number == body.number)
+  const isNumberRepeated = persons.find(
+    (person) => person.number == body.number
+  )
 
   if (!body.name) {
     return res.status(404).json({
-      error: "name missing"
+      error: 'name missing',
     })
-  } else if(!body.number) {
+  } else if (!body.number) {
     return res.status(404).json({
-      error: "phone number missed"
+      error: 'phone number missed',
     })
-  } else if(isNumberRepeated) {
+  } else if (isNumberRepeated) {
     return res.status(403).json({
-      error: "number must be unique"
+      error: 'number must be unique',
     })
   }
 
   const person = {
     id: newId(),
     name: body.name,
-    number: body.number 
+    number: body.number,
   }
   persons = persons.concat(person)
   res.json(person)
-
 })
 
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
+  persons = persons.filter((person) => person.id !== id)
 
   res.status(204).end()
 })
 
-
-
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 })
