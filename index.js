8880cf25cee0e7bf9ed8bcf8cf49bd7849193ab6
steven <alvaradosteven315@gmail.com>
2023-01-05 16:17:38 -0500

    working with get method for save data to mongodb

--- index.js
+++ index.js
@@ -6,29 +6,6 @@ const Person = require('./models/Persons')
 const cors = require('cors')
 app.use(cors())
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
-
 app.use(express.json())
 
 const date = new Date()
@@ -56,14 +33,9 @@ app.get('/api/info', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find((p) => p.id === id)
-
-  if (person) {
+  Person.findById(req.params.id).then((person) => {
     res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  })
 })
 
 app.post('/api/persons', (req, res) => {
@@ -86,13 +58,14 @@ app.post('/api/persons', (req, res) => {
     })
   }
 
-  const person = {
-    id: newId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
-  persons = persons.concat(person)
-  res.json(person)
+  })
+
+  person.save().then((savedPerson) => {
+    res.json(savedPerson)
+  })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
